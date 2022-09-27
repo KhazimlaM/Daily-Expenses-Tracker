797b9a7f4c61a3e87b5e897faac39ed1bc0acff9
Khazimla <20113497@rcconnect.co.za>
2022-09-27 16:35:11 +0200

    database changes

--- index.js
+++ index.js
@@ -3,51 +3,38 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const flash = require('express-flash');
 const bodyParser = require('body-parser');
-const Greetings = require('./greetings');
 const pgp = require('pg-promise')();
-const myRoutes = require('./routes/routes')
 const app = express();
+const DailyExpenses = require('./trackerdb');
 
 
 
-const DATABASE_URL = process.env.DATABASE_URL || "postgresql://khazimla:pg123@localhost:5432/trackers";
+app.engine('handlebars', exphbs.engine({ defaultLayout: '' }));
 
-const config = {
-  connectionString : DATABASE_URL
-}
-
-if (process.env.NODE_ENV == 'production') {
-  config.ssl = {
-    rejectUnauthorized: false
-  }
-}
-
-const db = pgp(config);
-console.log(db);
-
-
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
-
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
-app.use(session({
-  secret: 'codeforgeek',
-  saveUninitialized: true,
-  resave: true
-}));
+app.get('/', function (req, res) {
+    res.render('index',);
+});
 
-app.use(flash());
+app.post('/log', function (req, res) {
+    res.render('expenses',);
+});
+
+
+const dailyExpense = DailyExpenses();
 
 
 
-const PORT = process.env.PORT || 4040;
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
-  console.log("App started at port:", PORT)
+    console.log("App started at port:", PORT)
 });
 
---
+++ trackerdb.js
@@ -0,0 +1,21 @@
+module.exports = function DailyExpenses(db) {
+
+    async function setNames(name) {
+
+        var checkUser = await db.oneOrNone('select last_name from tracker where last_name = Khazimla ', [name])
+        console.log(checkUser)
+        if (checkUser === null) {
+            await db.none('insert into tracker(first_name,last_name, email) values(Khazimla )', [name, 1])
+
+        }
+
+
+    }
+
+    
+    return {
+    setNames,
+        
+    }
+
+}
