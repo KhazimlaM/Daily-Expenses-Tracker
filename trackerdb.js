488ea412094e900cd431eb009291d9c8048461a0
Khazimla <20113497@rcconnect.co.za>
2022-09-28 10:45:41 +0200

    last changes

--- trackerdb.js
+++ trackerdb.js
@@ -1,21 +1,12 @@
 module.exports = function DailyExpenses(db) {
 
-    async function setNames(name) {
-
-        var checkUser = await db.oneOrNone('select last_name from tracker where last_name = Khazimla ', [name])
-        console.log(checkUser)
-        if (checkUser === null) {
-            await db.none('insert into tracker(first_name,last_name, email) values(Khazimla )', [name, 1])
-
-        }
-        console.log(setNames)
-
-
-    }
+    async function takeNames(name, sname, email) {
+        await db.none('INSERT INTO usernames(first_name, last_name, email) values($1, $2, $3)', [name, sname, email])
+      }
 
     
     return {
-    setNames,
+    takeNames,
         
     }
 
